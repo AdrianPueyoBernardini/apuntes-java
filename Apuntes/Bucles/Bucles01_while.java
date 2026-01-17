19cb00bb8dc4e0765dc22566958b6dd0cda65867
adrianpueyobernardini <adrianpbvfx@gmail.com>
2026-01-17 19:12:56 +0100

    new content

---
+++ Apuntes/Bucles/Bucles01_while.java
@@ -0,0 +1,11 @@
+package Bucles;
+
+public class Bucles01_while {
+    public static void main(String[] args){
+        int myint = 0;
+        while (myint<5){
+            System.out.println(myint);
+            myint++;
+        }
+    }
+}
---
+++ Apuntes/Practicas/Practica06_ParesBucle.java
@@ -0,0 +1,14 @@
+package Practicas;
+
+public class Practica06_ParesBucle {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<21){
+            if(i%2==0){
+                System.out.println(i);
+            }
+
+            i++;
+        }
+    }
+}
